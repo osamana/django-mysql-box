ef4254d53a94f97ed6628f10c1991af7c07b0e05
osama <osamana@outlook.com>
2016-12-03 02:57:26 +0200

    modified to install django with mysql

--- provision_django.sh
+++ provision_django.sh
@@ -2,21 +2,6 @@
 # User level script to set up environment for Django development on Vagrant.
 # Target box: ubuntu/trusty64
 
-DB_NAME='mydjango'
-DB_USERNAME='mydjango'
-DB_PASSWORD='password'
-POSTGRES_PASSWORD='postgres'
-
-echo "---------------------------------------------"
-echo "Creating PostGIS database"
-echo "---------------------------------------------"
-sudo su - postgres << START
-psql -c "ALTER USER postgres WITH PASSWORD '$POSTGRES_PASSWORD';"
-createdb $DB_NAME
-psql -c "CREATE ROLE $DB_USERNAME WITH LOGIN ENCRYPTED PASSWORD '$DB_PASSWORD';"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USERNAME;"
-psql -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;" $DB_NAME
-START
 
 echo "---------------------------------------------"
 echo "Creating mydjango virtualenv"
@@ -47,15 +32,4 @@ npm install -g bower
 echo "---------------------------------------------"
 echo "Provisioning complete"
 echo "---------------------------------------------"
-printf "Use the following database connection settings in settings.py:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': '$DB_NAME',
-        'USER': '$DB_USERNAME',
-        'PASSWORD': '$DB_PASSWORD',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-"
+
